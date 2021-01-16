4fc3770ed9d46f33cbb36b572785dca7f86bdfa9
Patrick Beagan <pbeagan1@gmail.com>
2021-01-15 21:33:22 -0600

    Converting GA

---
+++ GA.kt
@@ -0,0 +1,23 @@
+object GA {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val pop = Population(50)
+        var gen = 0
+        var fit = 0
+        pop.initialize()
+        while (fit < 10000000) {
+            gen++
+            pop.selection(pop)
+            pop.crossover()
+            pop.mutation()
+            pop.populate()
+            fit = pop.evaluation()
+            println("Gen: $gen Fittest: $fit")
+        }
+        println("Found solution!")
+        println("Generation: $gen")
+        println("Genes:")
+        println("Invest " + pop.fittest.getx() + " in company X.")
+        println("Invest " + pop.fittest.gety() + " in company Y.")
+    }
+}
